1f356926f3efddc74887a7d9c86f52abcfaf7da2
Jessica D. Smith <jesmith@student.codam.nl>
2020-01-11 15:14:37 +0100

    adding unsaved file

--- srcs/fractol_manager.c
+++ srcs/fractol_manager.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/08 17:11:28 by jesmith        #+#    #+#                */
-/*   Updated: 2020/01/11 14:11:02 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/01/11 15:14:20 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,23 @@ void	fractol_draw(t_fractol *fractol)
 	iteration = 0;
 	while (points.x < WIDTH && points.y < HEIGHT)
 	{
-		number->new_real = 1.5 * (points.x - WIDTH / 2) / (0.5 * events.zoom * WIDTH) + events.mouse_x;
-		number->new_i = (points.y - HEIGHT / 2) / (0.5 * events.zoom * HEIGHT) + events.mouse_y;
-	}
-	while (iteration < MAX_ITERATIONS)
-	{
-		number->
-		iteration++;
+		number->new_real = 1.5 * (points.x - WIDTH / 2) / (0.5 * events.zoom * WIDTH) + events.mouse_x; // for Mandelbrot this is equal to number->real_pixel
+		number->new_i = (points.y - HEIGHT / 2) / (0.5 * events.zoom * HEIGHT) + events.mouse_y; // for Mandelbrot this is equal to number->i_pixel
+		// for Mandelbrot new_real, new_i, old_i, old_real are set to 0.0; 
+		while (iteration < MAX_ITERATIONS)
+		{
+			number->old_real = number->new_real;
+			number->old_i = number->new_i;
+			number->new_real = number->old_real * number->old_real - number->old_i * number->old_i + number->c_real; // + number->pixel_real
+			number->new_i = 2 * number->old_real * number->old_i + number->c_i; // + number->pixel_i
+			if ((number->new_real * number->new_real + number->new_i * number->new_i) > 4)
+				break ;
+			iteration++;
+		}
+		fractol->color = get_color(fractol);
+		put_pixel(fractol);
+		points.x++;
+		points.y++;
 	}
 }
 

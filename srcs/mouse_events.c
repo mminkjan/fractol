77a3b1b84456497f7a71d6400d74223429397d9c
Jessica D. Smith <jesmith@student.codam.nl>
2020-01-21 18:02:06 +0100

    better mouse_move

--- srcs/mouse_events.c
+++ srcs/mouse_events.c
@@ -6,7 +6,7 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/18 19:20:35 by jesmith        #+#    #+#                */
-/*   Updated: 2020/01/20 22:59:54 by jesmith       ########   odam.nl         */
+/*   Updated: 2020/01/21 17:32:10 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 
 int				mouse_press(int key, int x, int y, t_fractol *fractol)
 {
-	if (key == SCROLL_UP && fractol->event.zoom > 1.1)
-		fractol->event.zoom -= 1.1;
+	if (key == SCROLL_UP && fractol->event.zoom > 1.0)
+	{
+		fractol->event.zoom -= 1.0;
+		fractol_writer(fractol);
+	}
 	if (key == SCROLL_DOWN)
-		fractol->event.zoom += 1.1;
+	{
+		fractol->event.zoom += 1.0;
+		fractol_writer(fractol);
+	}
 	if (key == MOUSE_PRESS)
 	{
 		fractol->event.mouse_press = 1;
 		fractol->event.hold_x = x;
 		fractol->event.hold_y = y;
 	}
-	fractol_writer(fractol);
 	return (0);
 }
 
@@ -32,17 +37,20 @@ int				mouse_move(int x, int y, t_fractol *fractol)
 {
 	if (fractol->event.mouse_press == 1)
 	{
-		fractol->event.mouse_x += (fractol->event.hold_x - x) / WIDTH;
-		fractol->event.mouse_y += (fractol->event.hold_y - y) / HEIGHT;
+		fractol->event.mouse_x += \
+			(fractol->event.hold_x - x) / (WIDTH * fractol->event.zoom);
+		fractol->event.mouse_y += \
+			(fractol->event.hold_y - y) / (HEIGHT * fractol->event.zoom);
+		fractol_writer(fractol);
 	}
 	if (fractol->event.mouse_press == 0 && fractol->type == 1)
 	{
-		if (y < HEIGHT / 2)
+		if (y < HEIGHT / 2 || x < WIDTH)
 		{
 			fractol->numbers->c_real += 0.07;
 			fractol->numbers->c_i += 0.07;
 		}
-		else if (y > HEIGHT / 2)
+		else if (y > HEIGHT / 2 || x > WIDTH)
 		{
 			fractol->numbers->c_real -= 0.07;
 			fractol->numbers->c_i -= 0.07;
@@ -57,7 +65,11 @@ int				mouse_release(int key, int x, int y, t_fractol *fractol)
 	x = 0;
 	y = 0;
 	if (key == MOUSE_PRESS)
+	{
 		fractol->event.mouse_press = 0;
+		// fractol->event.hold_x = 0;
+		// fractol->event.hold_y = 0;
+	}
 	fractol_writer(fractol);
 	return (0);
 }
